728632a1525373ef6f1bbea2016e5f495d4fe55b
MrMan314 <mrman314dev@gmail.com>
2024-08-03 18:12:15 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, send_file
+from base64 import b64encode
+from io import BytesIO
+import replicate
+import urllib.request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	input = {
+		"prompt": "a meme about " + request.args.get("prompt") + " with comprehensible english text",
+		"output_quality": 100,
+		"disable_safety_checker": True
+	}
+
+	output = replicate.run(
+		"black-forest-labs/flux-schnell",
+		input=input
+	)
+	contents = urllib.request.urlopen(output).read()
+	return send_file(BytesIO(contents), mimetype="image/webp", download_name="generated.webp")
